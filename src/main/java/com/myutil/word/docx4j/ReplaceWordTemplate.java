eec35ea11a15a46e2b701036cb50bb1a38e9a08d
maozhanlei <maozhanlei@sina.com>
2015-11-20 10:31:33 +0800

    新增使用itext创建word文件，使用docx4j实现word模板替换的基本类

---
+++ src/main/java/com/myutil/word/docx4j/ReplaceWordTemplate.java
@@ -0,0 +1,19 @@
+/**
+* @author mzl
+* @FileName ReplaceWordTemplate.java
+* Created on 2015年11月20日 上午10:28:19
+* Copyright 2015 jhlc,Inc. All rights revserved.
+* 
+*/
+package com.myutil.word.docx4j;
+ 
+/** 
+ * 此类描述的是： 使用模板替换的方式实现word创建
+ * @author: maozhanlei/毛占磊
+ * @version: v1.0 
+ * @dateTime：2015年11月20日 上午10:28:22
+ */ 
+public class ReplaceWordTemplate {
+
+}
+  
---
+++ src/main/java/com/myutil/word/itext/CreateWord.java
@@ -0,0 +1,19 @@
+/**
+* @author mzl
+* @FileName CreateWord.java
+* Created on 2015年11月20日 上午10:27:33
+* Copyright 2015 jhlc,Inc. All rights revserved.
+* 
+*/
+package com.myutil.word.itext;
+ 
+/** 
+ * 此类描述的是： 使用itext实现word文件的创建
+ * @author: 毛占磊
+ * @version: v1.0 
+ * @dateTime：2015年11月20日 上午10:27:36
+ */ 
+public class CreateWord {
+
+}
+  
